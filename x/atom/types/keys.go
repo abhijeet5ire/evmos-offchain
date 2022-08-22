2c8513872ffba2213b6c67df901adaa034ce8e76
dung5ire <dung@5ire.org>
2022-08-22 16:54:32 +0900

    init commit
    Signed-off-by: dung5ire <dung@5ire.org>

---
+++ x/atom/types/keys.go
@@ -0,0 +1,25 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "atom"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for slashing
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	// QuerierRoute = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	// MemStoreKey = "mem_atom"
+
+	AtomUsdClaim = "AtomUsdClaim"
+
+)
+
+func KeyPrefix(p string) []byte {
+	return []byte(p)
+}
